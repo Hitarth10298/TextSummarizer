dbfc2e8e061debb893da8cc435cc83543bf184be
Hitarth Shah <hitarthshah10298@gmail.com>
2023-12-08 13:36:07 -0800

    Requirements & InstallationDone

--- setup.py
+++ setup.py
@@ -8,3 +8,19 @@ REPO_NAME = "TextSummarizer"
 AUTHOR_USER_NAME = "hitarth10298"
 SRC_REPO = "text_summarizer"
 AUTHOR_EMAIL = "hitarthshah10298@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
