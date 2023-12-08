9e00e42ceef198346a519d8e8dbed61bb4755c44
Hitarth Shah <hitarthshah10298@gmail.com>
2023-12-08 13:19:32 -0800

    Created Project Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+__version__ = "0.0.0"
+
+REPO_NAME = "TextSummarizer"
+AUTHOR_USER_NAME = "hitarth10298"
+SRC_REPO = "text_summarizer"
+AUTHOR_EMAIL = "hitarthshah10298@gmail.com"
